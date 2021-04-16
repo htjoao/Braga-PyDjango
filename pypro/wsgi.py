72b85e71eb89581216274906b3dc7f8cd4fa630a
Win <jvictor.braga@yahoo.com>
2021-04-16 13:11:14 -0300

    Ola Django

--- pypro/wsgi.py
+++ pypro/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pydjango project.
+WSGI config for pypro project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pydjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pypro.settings')
 
 application = get_wsgi_application()
